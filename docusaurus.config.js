f2e9c2900adcd9f1c14de332dd23965f681512f1
Boshen <boshenc@gmail.com>
2022-08-10 14:21:52 +0800

    add SEO metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      metadata: [{name: 'keywords', content: 'rust, javascript, compiler, tutorial'}],
       colorMode: {
         defaultMode: 'dark',
       },
