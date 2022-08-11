9fcb49782051aae1e44d2b02ebca8e91b905b0a8
Boshen <boshenc@gmail.com>
2022-08-11 10:17:38 +0800

    clean up docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,27 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Writing a JavaScript Compiler in Rust',
-  tagline: '',
+  title: 'Write a JavaScript Compiler in Rust',
   url: 'https://boshen.github.io',
   baseUrl: '/javascript-compiler-in-rust/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  // favicon: '',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  onBrokenMarkdownLinks: 'throw',
+  onDuplicateRoutes: 'throw',
   organizationName: 'Boshen', // Usually your GitHub org/user name.
   projectName: 'javascript-compiler-in-rust', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
       'classic',
@@ -34,17 +25,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/Boshen/javascript-compiler-in-rust/tree/main/docs/',
+          showReadingTime: true,
         },
         blog: {
+          editUrl: 'https://github.com/Boshen/javascript-compiler-in-rust/tree/main/blog',
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -62,10 +48,6 @@ const config = {
       },
       navbar: {
         title: 'Home',
-        // logo: {
-          // alt: 'My Site Logo',
-          // src: 'img/logo.svg',
-        // },
         items: [
           {
             type: 'doc',
@@ -81,51 +63,6 @@ const config = {
           },
         ],
       },
-      // footer: {
-        // style: 'dark',
-        // links: [
-          // {
-            // title: 'Docs',
-            // items: [
-              // {
-                // label: 'Tutorial',
-                // to: '/docs/intro',
-              // },
-            // ],
-          // },
-          // {
-            // title: 'Community',
-            // items: [
-              // {
-                // label: 'Stack Overflow',
-                // href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              // },
-              // {
-                // label: 'Discord',
-                // href: 'https://discordapp.com/invite/docusaurus',
-              // },
-              // {
-                // label: 'Twitter',
-                // href: 'https://twitter.com/docusaurus',
-              // },
-            // ],
-          // },
-          // {
-            // title: 'More',
-            // items: [
-              // {
-                // label: 'Blog',
-                // to: '/blog',
-              // },
-              // {
-                // label: 'GitHub',
-                // href: 'https://github.com/facebook/docusaurus',
-              // },
-            // ],
-          // },
-        // ],
-        // copyright: `Copyright © ${new Date().getFullYear()} Boshen, Inc. Built with Docusaurus.`,
-      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
