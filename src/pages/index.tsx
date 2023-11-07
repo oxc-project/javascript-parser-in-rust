c9c8875be71b5c3280864935326b017bb984e765
Boshen <boshenc@gmail.com>
2023-11-07 14:19:29 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,8 +22,7 @@ function HomepageHeader() {
         </p>
         <p className="hero__subtitle">
           This will be the guide for you if you are interested in learning Rust, parsers,
-          or would like to contribute to <a href="https://github.com/boshen/oxc" target="__blank">oxc</a>,
-          <a href="https://swc.rs" target="__blank">swc</a> or <a href="https://biomejs.dev" target="__blank">Biome</a> in the near future.
+          or would like to contribute to <a href="https://github.com/boshen/oxc" target="__blank">oxc</a>, <a href="https://swc.rs" target="__blank">swc</a> or <a href="https://biomejs.dev" target="__blank">Biome</a> in the near future.
         </p>
         <p className="hero__subtitle">
           <Translate id="homepage.subtitle.third">
